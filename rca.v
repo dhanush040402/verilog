99ce5e9573abbe983584252750ce8103b7c0fa0e
dhanush040402 <dhanushvelmurugan1@gmail.com>
2025-12-05 16:24:03 +0530

    Add files via upload

---
+++ rca.v
@@ -0,0 +1,18 @@
+module rca(
+    input [3:0] A,
+    input [3:0] B,
+    input Cin,
+    output reg [3:0] Sum,
+    output reg Cout
+);
+integer i;
+ reg [4:0] carry;
+ always @(*) begin
+        carry[0] = Cin;
+        for (i = 0; i < 4; i = i + 1) begin
+            Sum[i] = A[i] ^ B[i] ^ carry[i];
+            carry[i+1] = (A[i] & B[i]) | (A[i] & carry[i]) | (B[i] & carry[i]);
+        end
+        Cout = carry[4];
+    end
+endmodule
