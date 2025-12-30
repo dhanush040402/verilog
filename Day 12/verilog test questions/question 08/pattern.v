076f41c1fa4f510b954cc5c9c134a8d8a02c6a46
dhanush040402 <dhanushvelmurugan1@gmail.com>
2025-12-30 16:01:22 +0530

    Update pattern.v

--- Day 12/verilog test questions/question 08/pattern.v
+++ Day 12/verilog test questions/question 08/pattern.v
@@ -2,59 +2,48 @@
 
 
 
-
-module diamond_pattern;
+module pattern_print;
 
     integer i, j, k;
-    integer width;
-    integer num_width;
-    integer lead_spaces;
+    integer N = 5;
 
     initial begin
-        width = 9;   // max width: "5 5 5 5 5" = 9 characters
-
-        // Upper half including middle
-        for (i = 1; i <= 5; i = i + 1) begin
-            num_width  = 2*i - 1;
-            lead_spaces = (width - num_width)/2 + 4;
-
-            for (j = 0; j < lead_spaces; j = j + 1)
+        for (i = 1; i <= N; i = i + 1) begin
+            for (k = 1; k <= N - i; k = k + 1)
                 $write(" ");
+            for (j = 1; j <= i; j = j + 1)
+                $write("%0d ", i);
 
-            for (k = 1; k <= i; k = k + 1) begin
-                $write("%0d", i);
-                if (k < i) $write(" ");
-            end
             $write("\n");
         end
 
-        // Lower half
-        for (i = 4; i >= 1; i = i - 1) begin
-            num_width  = 2*i - 1;
-            lead_spaces = (width - num_width)/2 + 4;
 
-            for (j = 0; j < lead_spaces; j = j + 1)
+        for (i = N - 1; i >= 1; i = i - 1) begin
+            for (k = 1; k <= N - i; k = k + 1)
                 $write(" ");
+                for (j = 1; j <= i; j = j + 1)
+                $write("%0d ", i);
 
-            for (k = 1; k <= i; k = k + 1) begin
-                $write("%0d", i);
-                if (k < i) $write(" ");
-            end
             $write("\n");
         end
+
+        $finish;
     end
 
 endmodule
 
-/*output
-        1
-       2 2
-      3 3 3
-     4 4 4 4
-    5 5 5 5 5
-     4 4 4 4
-      3 3 3
-       2 2
-        1
 
-	 */
+
+/*Output 
+*
+    1 
+   2 2 
+  3 3 3 
+ 4 4 4 4 
+5 5 5 5 5 
+ 4 4 4 4 
+  3 3 3 
+   2 2 
+    1 
+*/
+
