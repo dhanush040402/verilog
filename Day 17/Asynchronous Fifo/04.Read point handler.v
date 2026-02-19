bbfc907b757ed3744427d73cd3c2021cd52a78da
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-19 17:42:56 +0530

    Update 04.Read point handler.v

--- Day 17/Asynchronous Fifo/04.Read point handler.v
+++ Day 17/Asynchronous Fifo/04.Read point handler.v
@@ -1,33 +1,32 @@
+`timescale 1ns/1ps
+
 module rptr_handler #(parameter ptr_width = 3)(
   input rclk,rrst,r_en,
   input [ptr_width:0]g_wptr_sync,
   output reg empty,
   output reg [ptr_width:0]b_rptr,g_rptr
 );
-  reg[ptr_width:0]b_rptr_next;
-  reg[ptr_width:0]g_rptr_next;
+  wire [ptr_width:0]b_rptr_next;
+  wire [ptr_width:0]g_rptr_next;
   
   wire rempty;
   
   assign b_rptr_next=b_rptr+(r_en && !empty);
   assign g_rptr_next=(b_rptr_next >>1) ^ b_rptr_next;
-  assign rempty=(g_rptr_next == g_wptr_sync);
   
-    always @(posedge rclk or negedge rrst)begin
-      if(!rrst)begin
+  always @(posedge rclk or negedge rrst)begin
+    if(!rrst)begin
       b_rptr<=0;
       g_rptr<=0;
+      empty<=1;
     end
     else begin
       b_rptr<=b_rptr_next;
       g_rptr<=g_rptr_next;
+      empty<=rempty;
     end
   end
   
-  always @(posedge rclk or negedge rrst)begin
-    if(!rrst)
-      empty<=1;
-    else 
-      empty<=rempty;
-  end
+  assign rempty=(g_rptr_next == g_wptr_sync);
+  
 endmodule
