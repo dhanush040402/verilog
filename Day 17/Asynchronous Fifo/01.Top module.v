2a8b00cb83150d79d68d6e4eec42816539646251
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-03-18 21:55:30 +0530

    Update 01.Top module.v

--- Day 17/Asynchronous Fifo/01.Top module.v
+++ Day 17/Asynchronous Fifo/01.Top module.v
@@ -1,68 +1,69 @@
-//design
 `timescale 1ns/1ps
 
-`include "synchronizer.sv"
-`include "write_point_handler.sv"
-`include "read_point_handler.sv"
-`include "Fifo_memory.sv"
+`include "synchronizer.v"
+`include "write_ptr_handler.v"
+`include "read_ptr_handler.v"
+`include "memory.v"
 
-module asynchronous_fifo #(parameter depth = 8 , data_width = 8 ) (
-  input wclk,wrst,w_en,rclk,rrst,r_en,
-  input [data_width-1:0]data_in,
-  output full,empty,
-  output [data_width-1:0]data_out
+module async_fifo_top #(parameter depth=8, data_width=8) (
+  input wclk, wrst, w_en,
+  input rclk, rrst, r_en,
+  input [data_width-1:0] data_in,
+  output [data_width-1:0] data_out,
+  output full, empty
 );
+  
   parameter ptr_width = $clog2(depth);
   
-  wire [ptr_width:0]b_wptr,b_rptr,g_wptr,g_rptr,g_wptr_sync,g_rptr_sync;
-  
+  wire [ptr_width:0] b_wptr, b_rptr;
+  wire [ptr_width:0] g_wptr, g_rptr;
+  wire [ptr_width:0] g_wptr_sync, g_rptr_sync;
   
   synchronizer #(ptr_width) sync_wptr(
     .clk(rclk),
     .rst(rrst),
-    .gin(g_wptr),
+    .gin(g_wptr), 
     .gout(g_wptr_sync)
   );
   
   synchronizer #(ptr_width) sync_rptr(
     .clk(wclk),
     .rst(wrst),
-    .gin(g_rptr),
+    .gin(g_rptr), 
     .gout(g_rptr_sync)
   );
   
-  wptr_handler #(ptr_width) write_h(
-    .wclk(wclk),
-    .wrst(wrst),
-    .w_en(w_en),
-    .g_rptr_sync(g_rptr_sync),
-    .b_wptr(b_wptr),
-    .g_wptr(g_wptr),
+  write_ptr_handler #(ptr_width) w_ptr_h (
+    .wclk(wclk), 
+    .wrst(wrst), 
+    .w_en(w_en), 
+    .g_rptr_sync(g_rptr_sync), 
+    .b_wptr(b_wptr), 
+    .g_wptr(g_wptr), 
     .full(full)
   );
   
-  rptr_handler #(ptr_width) read_h(
-    .rclk(rclk),    
-    .rrst(rrst),
-    .r_en(r_en),
-    .g_wptr_sync(g_wptr_sync),
-    .b_rptr(b_rptr),
-    .g_rptr(g_rptr),
+  read_ptr_handler #(ptr_width) r_ptr_h (
+    .rclk(rclk), 
+    .rrst(rrst), 
+    .r_en(r_en), 
+    .g_wptr_sync(g_wptr_sync), 
+    .b_rptr(b_rptr), 
+    .g_rptr(g_rptr), 
     .empty(empty)
   );
   
-  
-  fifo_memory #(depth,data_width,ptr_width) fifo(
-    .wclk(wclk),
-    .w_en(w_en),
-    .rclk(rclk),
-    .r_en(r_en),
-    .full(full),
-    .empty(empty),
-    .data_in(data_in),
-    .data_out(data_out),
-    .b_wptr(b_wptr),
-    .b_rptr(b_rptr)
+  memory #(depth, data_width, ptr_width) fifo (
+    .wclk(wclk), 
+    .rclk(rclk), 
+    .w_en(w_en), 
+    .r_en(r_en), 
+    .full(full), 
+    .empty(empty), 
+    .b_wptr(b_wptr), 
+    .b_rptr(b_rptr), 
+    .data_in(data_in), 
+    .data_out(data_out)
   );
   
 endmodule
